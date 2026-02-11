13ba9f76dee837023f7b1521b7426d28015f7503
AaronCrisologo <131829586+AaronCrisologo@users.noreply.github.com>
2026-02-11 22:09:01 +0800

    Update tweet_data text for simulation mode

--- bot.py
+++ bot.py
@@ -113,7 +113,7 @@ def main():
     # === HARDCODED DATA SIMULATION ===
     if SIMULATION_MODE:
         tweet_data = {
-            'text': "TEST: This post is dense with emojis! 🚀🔥✨🌈🍄💠🌀🧿🔮🎭🎨🎰🎳🎯🎬🎤🎧🎫🎟️🏅🥇🥈🥉🏆🏏🏑🏒🏓🏸🥊🥋🥅⛳️⛸️🎣🚣🏊🏄🛀⛹️🏋️🚴🚵🏎️🏍️🤸🤼🤽🤾🤺🤹🧗🧘🧖🛌👯🗣️👤👥🤺 If you see this at the end of the post, the truncation failed to trigger early enough for byte safety. 🛑",
+            'text': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla, ante nec tincidunt congue, neque sem pulvinar justo, vitae tempus nulla ipsum a magna. Quisque dapibus arcu ligula, at mollis nulla interdum vitae. Vestibulum lobortis euismod faucibus. Proin et ornare tortor. Integer fringilla nunc nec tortor dignissim, eu dictum est maximus. Nunc sit amet sapien convallis, tristique sapien in, luctus nulla. Integer convallis vulputate ante ac maximus. Aliquam malesuada auctor massa nunc.",
             'time': datetime.datetime.now(datetime.timezone.utc).isoformat(),
             'images': [], # You can add local paths here if you have test images
             'hasVideo': False
