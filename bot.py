557ebfd8b7f1eefa674bfd99088eb38dd74d26cd
AaronCrisologo <131829586+AaronCrisologo@users.noreply.github.com>
2026-03-05 13:41:36 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -183,7 +183,7 @@ def main():
 #FateGOUS""",
             'time': datetime.datetime.now(datetime.timezone.utc).isoformat(),
             'images': [],
-            'hasVideo': False
+            'hasVideo': True
         }
     else:
         print(f"\n[{datetime.datetime.now(datetime.timezone.utc)}] Running Production...")
