26e959e830f2b4701dde948d9489facd57ab02c0
AaronCrisologo <131829586+AaronCrisologo@users.noreply.github.com>
2026-03-05 14:26:54 +0800

    Update fallbacks.py

--- fallbacks.py
+++ fallbacks.py
@@ -10,6 +10,7 @@ TEMP_MANGA_PATH = "temp_manga.jpg"  # Temporary file for downloaded manga
 SERVANTS_MAP = {
     "medusa (saber)": {"img": "Medusa (Saber).jpg", "alt": "Medusa (Saber) Pickup Summon"},
     "durga": {"img": "Durga.jpg", "alt": "Durga Pickup Summon"},
+    "sigurd": {"img": "Sigurd.jpg", "alt": "Sigurd Pickup Summon"},
     "bhima": {"img": "Bhima.jpg", "alt": "Bhima Pickup Summon"},
     "bakin": {"img": "bakin.png", "alt": "Kyokutei Bakin Pickup Summon"},
     "suzuka gozen (rider)": {"img": "vacay.jpg", "alt": "Suzuka Gozen (Rider) Pickup Summon"},
